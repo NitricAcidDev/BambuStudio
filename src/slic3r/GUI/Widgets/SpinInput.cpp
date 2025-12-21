0fe7034d34d13a677170979995c4aebb7571708b
Nitric Acid <73795881+NitricAcidDev@users.noreply.github.com>
2025-12-21 02:42:44 +0000

    Themeify SpinInput hover color

--- src/slic3r/GUI/Widgets/SpinInput.cpp
+++ src/slic3r/GUI/Widgets/SpinInput.cpp
@@ -2,6 +2,7 @@
 #include "Label.hpp"
 #include "Button.hpp"
 #include "TextCtrl.h"
+#include "../GUI_App.hpp"
 
 #include <wx/dcgraph.h>
 
@@ -20,13 +21,21 @@ END_EVENT_TABLE()
  * calling Refresh()/Update().
  */
 
+static wxColour ThemeGreen()
+{
+    auto *app = dynamic_cast<Slic3r::GUI::GUI_App*>(&Slic3r::GUI::wxGetApp());
+    if (app && app->IsMainLoopRunning())
+        return app->get_theme_colors().button_green.colorForStates(StateColor::Hovered | StateColor::Enabled);
+    return wxColour(0, 174, 66);
+}
+
 SpinInput::SpinInput()
     : label_color(std::make_pair(0x909090, (int) StateColor::Disabled), std::make_pair(0x6B6B6B, (int) StateColor::Normal))
     , text_color(std::make_pair(0x909090, (int) StateColor::Disabled), std::make_pair(0x262E30, (int) StateColor::Normal))
 {
     radius = 0;
     border_width     = 1;
-    border_color     = StateColor(std::make_pair(0xDBDBDB, (int) StateColor::Disabled), std::make_pair(0x00AE42, (int) StateColor::Hovered),
+    border_color     = StateColor(std::make_pair(0xDBDBDB, (int) StateColor::Disabled), std::make_pair(ThemeGreen(), (int) StateColor::Hovered),
                               std::make_pair(0xDBDBDB, (int) StateColor::Normal));
     background_color = StateColor(std::make_pair(0xF0F0F1, (int) StateColor::Disabled), std::make_pair(*wxWHITE, (int) StateColor::Normal));
 }
